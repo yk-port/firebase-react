7a348974908008a8e0db44525dc7a24e71246af5
yokokura <yokokura0413@gmail.com>
2020-04-02 19:10:44 +0900

    フォームの作成

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import { Container } from 'react-bootstrap';
+import { Container, Form, FormControl, InputGroup, Button } from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
       <Container>
-        <h1>ToDoリスト</h1>
+        <h1 className="mt-4">ToDoリスト</h1>
+        <Form>
+          <InputGroup className="mb-3">
+            <FormControl
+              type="text"
+              value=""
+            />
+            <InputGroup.Append addonType="append">
+              <Button type="submit" variant="secondary">追加</Button>
+            </InputGroup.Append>
+          </InputGroup>
+        </Form>
       </Container>
     </div>
   );
