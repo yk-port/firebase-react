dd104ea75e62d8e5372fb7043022bcc90c4fdb32
yokokura <yokokura0413@gmail.com>
2020-04-02 21:42:38 +0900

    feat: form

--- src/App.js
+++ src/App.js
@@ -1,20 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import { Container, Form, FormControl, InputGroup, Button } from 'react-bootstrap';
 
 function App() {
+  const [value, setValue] = useState('');
+  const [todos, setTodos] = useState([]);
+
+  const hundleSubmit = e => {
+    e.preventDefault();
+    addTodo(value);
+  }
+
+  const addTodo = text => {
+    const newTodos = [...todos, text];
+    setTodos(newTodos);
+    console.log(newTodos);
+  }
+
   return (
     <div className="App">
       <Container>
         <h1 className="mt-4">ToDoリスト</h1>
-        <Form>
+        <Form onSubmit={hundleSubmit}>
           <InputGroup className="mb-3">
             <FormControl
               type="text"
-              value=""
-            />
-            <InputGroup.Append addonType="append">
+              value={value}
+              onChange={e => setValue(e.target.value)} />
+            <InputGroup.Append>
               <Button type="submit" variant="secondary">追加</Button>
             </InputGroup.Append>
           </InputGroup>
